298773fdd477d6e061a5782c11b94c8945d1ac5c
srike27 <srike27@gmail.com>
2019-02-25 02:47:56 +0530

    added more geometric funvtions

--- src/geometry.py
+++ src/geometry.py
@@ -27,7 +27,116 @@ class vorenoiteam:
         self.r0 = point(r0.x,r0.y)
         self.r1 = point(r1.x,r1.y)
 
-def midpt(d1,d2):
+def ptoposeconv(pt,ang):
+    pose = Pose()
+    pose.position.x = pt.x
+    pose.position.y = pt.y
+    pose.orientation.z = m.tan(ang/2)
+    pose.orientation.w = 1
+    return pose
+
+def potwalls(pt):
+    lbda = 0.001
+    x = pt.x
+    y = pt.y
+
+    l = 980
+    w = 720
+
+    lby2 = l/2.0
+    wby2 = w/2.0
+
+    taplha1 = (lby2 - x)/(wby2 - y)
+    taplha2 = (wby2 - y)/(lby2 - x)
+    taplha3 = (lby2 + x)/(wby2 + y)
+    taplha4 = (wby2 + y)/(lby2 - x)
+    tbeta1 = (lby2 + x)/(wby2 - y)
+    tbeta2 = (wby2 + y)/(lby2 - x)
+    tbeta3 = (lby2 - x)/(wby2 + y)
+    tbeta4 = (wby2 - y)/(lby2 - x)
+
+    pot1 = np.log(taplha1*tbeta1)
+    pot2 = np.log(taplha2*tbeta2)
+    pot3 = np.log(taplha3*tbeta3)
+    pot4 = np.log(taplha4*tbeta4)
+
+    potf = pot1 + pot2 + pot3 + pot4
+
+    return lbda*potf
+
+def findmingrad(own,op1,op2,te1,goal):
+    #print goal.x
+    #print goal.y
+    #print own.x,own.y,op1.x,op1.y,op2.x,op2.y,te1.x,te1.y
+    mini = 1000000000000000
+    ang = float('nan')
+    for i in range(101):
+        theta = i*2*3.14/100
+        pt = point(0,0)
+        pt.x = own.x + 40*m.cos(theta)
+        pt.y = own.y + 40*m.sin(theta)
+        potw = potwalls(pt)
+        #print potw
+        pot1 = findpot(pt,op1,50)
+        #print pot1
+        pot2 = findpot(pt,op2,50)
+        #print pot2
+        gpot = findpot(goal,pt,-30)
+        #print gpot
+        potf = -gpot + pot1 + pot2
+        if (potf < mini):
+            mini = potf
+            ang = theta
+    #print 'ang',ang
+    return ang+3.14
+
+def findpot(pt1,pt2,q):
+    d = distb(pt1,pt2)
+    pot =  q/d    
+    return pot
+
+def posetoptconv(pose):
+    pt = point(pose.position.x,pose.position.y)
+    ang = pose.orientation.z
+    ang = 2*m.atan(ang)
+    return pt,ang
+
+def distb(p1,p2):
+    return m.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y))
+
+def angb(p1,p2):
+    c = p2.x-p1.X
+    c /= distb(p1,p2)
+    s = p2.y - p1.y
+    s /= distb(p1,p2)
+    ang = m.atan2(s,c)
+    return -ang
+
+class vect:
+    def __init__(self,d1,d2): #angle from d1 to d2
+        self.tail = d1
+        self.head = d2 
+        self.mag = distb(d1,d2)
+        self.ang = angb(d1,d2)
+        self.xcomp = m.cos(self.ang)
+        self.ycomp = m.sin(self.ang)
+
+    def getline(self,pt):
+        m = m.tan(self.ang)
+        l1 = line(type = 2,d1 = m, d2 = pt)
+        return l1
+
+def dotprod(v1,v2):
+    dp = v1.xcomp*v2.xcomp + v1.ycomp*v2.ycomp
+    dp = v1.mag* v2.mag * dp
+    return dp
+
+def crosprod(v1,v2):
+    cp = v1.xcomp*v2.ycomp -  v1.ycomp*v2.xcomp
+    cp = v1.mag*v2.mag*cp
+    return cp
+
+def midpt(d1,d2): 
     x = d1.x + d2.x
     y = d1.y + d2.y
     x = x/2
