4e846e8dd7942eff831281cfd3af07309d461f06
Srikrishna <srike27@gmail.com>
2019-12-15 19:45:01 +0530

    added 3 way input and multiplexed singular output

--- src/ik.py
+++ src/ik.py
@@ -34,6 +34,62 @@ r2=[p.robot(x=0,y=0,ball=ball),p.robot(x=0,y=0,ball=ball)]
 gs = 0
 
 phw = rospy.Publisher('wheel_vel_cmd',hw,queue_size=20)
+r10cs = 1
+r11cs = 1
+r20cs = 1
+r21cs = 1
+
+def r10selcallback(msg):
+    global r10cs
+    if msg.data == 3:
+        r10cs = 3
+    else:
+        r10cs = 0
+    return 0
+
+def r11selcallback(msg):
+    global r11cs
+    if msg.data == 3:
+        r11cs = 3
+    else:
+        r11cs = 0
+    return 0
+
+def r20selcallback(msg):
+    global r20cs
+    if msg.data == 3:
+        r20cs = 3
+    else:
+        r20cs = 0
+    return 0
+
+def r21selcallback(msg):
+    global r21cs
+    if msg.data == 3:
+        r21cs = 3
+    else:
+        r21cs = 0
+    return 0
+
+def exctrl10callback(msg):
+    global phw
+    phw.publish(msg)
+    return 0
+
+def exctrl11callback(msg):
+    global phw
+    phw.publish(msg)
+    return 0
+
+def exctrl20callback(msg):
+    global phw
+    phw.publish(msg)
+    return 0
+
+def exctrl21callback(msg):
+    global phw
+    phw.publish(msg)
+    return 0
 
 def botcallback1(msg):
     global r1
@@ -78,6 +134,10 @@ def ctrlcallback(msg):
     global r1
     global r2
     global ball
+    global r10cs
+    global r11cs
+    global r20cs
+    global r21cs
     R = 46.6  # radius of robot in pixels
     r = 15.7 # radius of wheels in pixels
     vgx = msg.kx
@@ -85,27 +145,38 @@ def ctrlcallback(msg):
     wz = msg.thetad
     tag = msg.tag
     kick = msg.kick
+    flag = 0
     # global to local transformations
     if tag==0:
+        if not r10cs == 3: #checking if not in extremal control mode
+            flag = 1
         bcos = m.cos(r1[0].theta)
         bsin = m.sin(r1[0].theta)
     if tag==1:
+        if not r11cs == 3: #checking if not in extremal control mode
+            flag = 1
         bcos = m.cos(r1[1].theta)
         bsin = m.sin(r1[1].theta)
     if tag==2:
+        if not r20cs == 3: #checking if not in extremal control mode
+            flag = 1
         bcos = m.cos(r2[0].theta)
         bsin = m.sin(r2[0].theta)
     if tag==3:
+        if not r21cs == 3: #checking if not in extremal control mode
+            flag = 1
         bcos = m.cos(r2[1].theta)
         bsin = m.sin(r2[1].theta)
-    vx = vgx*bcos + vgy*bsin
-    vy = -vgx*bsin + vgy*bcos
-    # inverse kinematics of 3 wheeled system
-    w1 = ((vx/1.732)/r) - ((vy/3)/r) + ((wz*R)/r)
-    w2 = ((vy/1.5)/r) + ((R*wz)/r)
-    w3 = ((wz*R)/r) - ((vx/1.732)/r) - ((vy/3)/r)
-    a = hw(w1=w1,w2=w2,w3=w3,tag=tag,kick=kick)
-    phw.publish(a)
+    if flag == 1:
+        vx = vgx*bcos + vgy*bsin
+        vy = -vgx*bsin + vgy*bcos
+        # inverse kinematics of 3 wheeled system
+        w1 = ((vx/1.732)/r) - ((vy/3)/r) + ((wz*R)/r)
+        w2 = ((vy/1.5)/r) + ((R*wz)/r)
+        w3 = ((wz*R)/r) - ((vx/1.732)/r) - ((vy/3)/r)
+        a = hw(w1=w1,w2=w2,w3=w3,tag=tag,kick=kick)
+        phw.publish(a)
+    return 0
 
 def gamefun():
     rospy.spin()
@@ -113,10 +184,18 @@ def gamefun():
 if __name__ == '__main__':
     rospy.init_node('ik',anonymous=True)
     rospy.Subscriber('ctrl',game,ctrlcallback)
+    rospy.Subscriber('robot1n0/extremal_ctrl',hw,exctrl10callback)
+    rospy.Subscriber('robot1n1/extremal_ctrl',hw,exctrl11callback)
+    rospy.Subscriber('robot2n0/extremal_ctrl',hw,exctrl20callback)
+    rospy.Subscriber('robot2n1/extremal_ctrl',hw,exctrl21callback)
     rospy.Subscriber('robot1n0/pose', Pose, botcallback1)  
     rospy.Subscriber('robot1n1/pose', Pose, botcallback2)  
     rospy.Subscriber('robot2n0/pose', Pose, botcallback3)  
     rospy.Subscriber('robot2n1/pose', Pose, botcallback4)  
+    rospy.Subscriber('robot1n0/cselect',Int32,r10selcallback)
+    rospy.Subscriber('robot1n1/cselect',Int32,r11selcallback)
+    rospy.Subscriber('robot2n0/cselect',Int32,r20selcallback)
+    rospy.Subscriber('robot2n1/cselect',Int32,r21selcallback)
     try:
         gamefun()
     except rospy.ROSInterruptException:
--- src/p2pctrl.py
+++ src/p2pctrl.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import sys
 import physics as p
-import pygame as pg
 import rospy
 import math as m
 from geometry_msgs.msg import Pose, Twist
@@ -16,9 +15,15 @@ if gmsg.status == 0 bot is stationary
 if gmsg.status == 1 bot moves to location in gmsg
 if gmsg.status == 2 ball kicked at target location
 
+
+if rxycs == 1 p2p control pass
+if rxycs == 2 trajectory command bypass
+if rxycs == 3 extremal control
 """
+
 flag=0
 ctrl=game()
+traj_ctrl = game()
 ctrl.kick=0
 ball = p.ball(x = 0,y = 0)
 bpose = Pose()
@@ -29,10 +34,10 @@ r10msg = goalmsg()
 r11msg = goalmsg()
 r20msg = goalmsg()
 r21msg = goalmsg() 
-r10cs = 0
-r11cs = 0
-r20cs = 0
-r21cs = 0
+r10cs = 1
+r11cs = 1
+r20cs = 1
+r21cs = 1
 r1f = 1
 r2f = 1
 r3f =1
@@ -123,28 +128,52 @@ def pid(xtg,ytg,bot,thtg=0):
     ctrl.tag=tag
     pid_ctrl.publish(ctrl)
 
+def r10tcallback(msg):
+    global r10cs
+    global pid_ctrl
+    if r10cs == 2:
+        pid_ctrl.publish(msg)
+    return 0
+
+def r11tcallback(msg):
+    global r11cs
+    global pid_ctrl
+    if r11cs == 2:
+        pid_ctrl.publish(msg)
+    return 0
+
+def r20tcallback(msg):
+    global r20cs
+    global pid_ctrl
+    if r20cs == 2:
+        pid_ctrl.publish(msg)
+    return 0
+
+def r21tcallback(msg):
+    global r21cs
+    global pid_ctrl
+    if r21cs == 2:
+        pid_ctrl.publish(msg)
+    return 0
+
 def r10selcallback(msg):
     global r10cs
-    if msg.data == 1:
-        r10cs = 1
+    r10cs = msg.data
     return 0
 
 def r11selcallback(msg):
     global r11cs
-    if msg.data == 1:
-        r11cs = 1
+    r11cs = msg.data
     return 0
 
 def r20selcallback(msg):
     global r20cs
-    if msg.data == 1:
-        r20cs = 1
+    r20cs = msg.data
     return 0
 
 def r21selcallback(msg):
     global r21cs
-    if msg.data == 1:
-        r21cs = 1
+    r21cs = msg.data
     return 0
 
 def balltwistcallback(msg):
@@ -299,10 +328,14 @@ def control(gmsg,robot,ball):
 def run():
     rospy.Subscriber('game/status',Int32,rulecheck)
     rospy.Subscriber('game/dribbler',Int32,dribcallback)
-    rospy.Subscriber('robot1n0/ptg',goalmsg,r10callback)
-    rospy.Subscriber('robot1n1/ptg',goalmsg,r11callback)
-    rospy.Subscriber('robot2n0/ptg',goalmsg,r20callback)
-    rospy.Subscriber('robot2n1/ptg',goalmsg,r21callback)publish
+    rospy.Subscriber('robot1n0/ptg',goalmsg,r10callback) # for native to node p2p control
+    rospy.Subscriber('robot1n1/ptg',goalmsg,r11callback) # for native to node p2p control
+    rospy.Subscriber('robot2n0/ptg',goalmsg,r20callback) # for native to node p2p control
+    rospy.Subscriber('robot2n1/ptg',goalmsg,r21callback) # for native to node p2p control
+    rospy.Subscriber('robot1n0/traj_vect',game,r10tcallback) # for the trajectory node bypass
+    rospy.Subscriber('robot1n1/traj_vect',game,r11tcallback) # for the trajectory node bypass
+    rospy.Subscriber('robot2n0/traj_vect',game,r20tcallback) # for the trajectory node bypass
+    rospy.Subscriber('robot2n1/traj_vect',game,r21tcallback) # for the trajectory node bypass
     rospy.Subscriber('ballpose', Pose,ballposecallback)
     rospy.Subscriber('balltwist',Twist,balltwistcallback)
     rospy.Subscriber('robot1n0/pose',Pose,r10posecallback)
